2b7c139e7b6bdcc30e111fb06096130c4a138477
amir <amirtaki623@gmail.com>
2025-10-07 23:12:43 +0330

    part 9

--- src/main.js
+++ src/main.js
@@ -1,1 +1,4 @@
 const x = document.querySelector('#container')
+const x = document.querySelector('#capture-button')
+const x = document.querySelector('#preview-container')
+const x = document.querySelector('#download-button')
